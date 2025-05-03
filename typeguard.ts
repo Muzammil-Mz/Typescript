e50e38187694ed4b158e82facff9481dbacffc3f
muzammil-mz <mujjuimran74@gmail.com>
2025-05-03 17:53:49 +0000

    added

--- typeguard.ts
+++ typeguard.ts
@@ -1,4 +1,4 @@
-//In TypeScript, the type guards are used to determine a variable's type, often inside a conditional or functional block. The type guards usually take the variable and return a Boolean value or the variable type. Type guards allow you to tell the TypeScript compiler to infer a given type for a variable in a 
+//In TypeScript, the type guards are used to determine a variable's type, often inside a conditional or functional block. The type guards usually take the variable and return a Boolean value or the variable type. Type guards allow you to tell the TypeScript compiler to infer a given type for a variable in a
 // specific context, guaranteeing that an argument's type is what you say it is.
 
 // typepf
@@ -18,12 +18,12 @@
 
 // Undefined
 
-let my_number: number = 123
-let my_string: string = 'Tutorialspoint'
-let my_boolean: boolean = true
-let my_object: { id: number } = { id: 1 }
+let my_number: number = 123;
+let my_string: string = "Tutorialspoint";
+let my_boolean: boolean = true;
+let my_object: { id: number } = { id: 1 };
 
-console.log('type of my_number variable is: ' + typeof my_number)
-console.log('type of my_string variable is: ' + typeof my_string)
-console.log('type of my_boolean variable is: ' + typeof my_boolean)
-console.log('type of my_object variable is: ' + typeof my_object)
+console.log("type of my_number variable is: " + typeof my_number);
+console.log("type of my_string variable is: " + typeof my_string);
+console.log("type of my_boolean variable is: " + typeof my_boolean);
+console.log("type of my_object variable is: " + typeof my_object);
